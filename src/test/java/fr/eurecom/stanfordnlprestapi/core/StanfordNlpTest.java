d3e134a22b09cd7024d0eee3b44dbde41bf4bc1c
Julien Plu <julien.plu@redaction-developpez.com>
2016-09-01 17:21:53 +0200

    Bugfix

--- src/test/java/fr/eurecom/stanfordnlprestapi/core/StanfordNlpTest.java
+++ src/test/java/fr/eurecom/stanfordnlprestapi/core/StanfordNlpTest.java
@@ -64,7 +64,7 @@ public class StanfordNlpTest {
     final Properties props = new Properties();
 
     props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, mention, coref");
-    props.setProperty("pos.model", "models/english-bidirectional-distsim.tagger");
+    props.setProperty("pos.model", "edu/stanford/nlp/models/pos-tagger/english-bidirectional/english-bidirectional-distsim.tagger");
     props.setProperty("ner.model",
         "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz");
     props.setProperty("ner.useSUTime", "false");
